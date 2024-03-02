afde0dc7374a18fe0be711d2a416dcca5eb9cb15
NguyenVanLuc290303 <127205559+NguyenVanLuc290303@users.noreply.github.com>
2024-03-02 10:44:46 +0700

    luc

--- src/config/api-consts.js
+++ src/config/api-consts.js
@@ -2,24 +2,28 @@
 
 import { Children } from "react";
 
-var ip_address = "192.168.1.231";
-
-export const API_ADD_USERS = "http://192.168.0.102:3000/api-taikhoan";
-
-export const API_LOGIN = "http://192.168.0.102:3000/api-taikhoan";
+export const IP_ADDRESS = "192.168.50.125";
 
 
-export const API_CATEGORY_PRODUCT = `http://192.168.0.102:3000/api-theloai`;
 
-export const API_PRODUCT = "http://192.168.0.102:3000/api-sanpham";
+export const API_ADD_USERS = `http://${IP_ADDRESS}:3000/api-taikhoan`;
 
-export const API_PRODUCT_TOP8 = "http://192.168.0.102:3000/api-sanpham/top8";
-
-export const API_COLOR_PRODUCT = "http://192.168.0.102:3000/api-mausanpham";
+export const API_LOGIN = `http://${IP_ADDRESS}:3000/api-taikhoan`;
 
 
-export const API_ORDER = "http://192.168.0.102:3000/api-donhang";
+export const API_CATEGORY_PRODUCT = `http://${IP_ADDRESS}:3000/api-theloai`;
+
+export const API_PRODUCT = `http://${IP_ADDRESS}:3000/api-sanpham`;
+
+export const API_PRODUCT_TOP8 = `http://${IP_ADDRESS}:3000/api-sanpham/top8`;
+
+export const API_COLOR_PRODUCT = `http://${IP_ADDRESS}:3000/api-mausanpham`;
 
 
-export const API_PRODUCT_ORDER = "http://192.168.0.102:3000/api-sanpham/SPD"
+export const API_ORDER = `http://${IP_ADDRESS}:3000/api-donhang`;
+
+
+export const API_PRODUCT_ORDER = `http://${IP_ADDRESS}:3000/api-sanpham/SPD`;
+
+
 
