4ab4b1cf8da0a609de582220d708f1b7aad5c053
Namtruong <118741740+namtruong0611@users.noreply.github.com>
2024-03-07 20:29:21 +0700

    forgotpassword

--- src/Screens/ForgotPassword/ForgotPassword.jsx
+++ src/Screens/ForgotPassword/ForgotPassword.jsx
@@ -9,8 +9,42 @@ import {
 import {Icons} from '../../constants/images';
 import {FontText} from '../../constants/Constant';
 import COLORS from '../../constants/colors';
+import auth from '@react-native-firebase/auth';
+import {useState} from 'react';
 
 const ForgotPassword = ({navigation}) => {
+  const [phoneNumber, setPhoneNumber] = useState();
+
+  function onAuthStateChanged(user) {
+    if (user) {
+      // Some Android devices can automatically process the verification code (OTP) message, and the user would NOT need to enter the code.
+      // Actually, if he/she tries to enter it, he/she will get an error message because the code was already used in the background.
+      // In this function, make sure you hide the component(s) for entering the code and/or navigate away from this screen.
+      // It is also recommended to display a message to the user informing him/her that he/she has successfully logged in.
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const handerForgotPassword = async () => {
+    try {
+      const confirmationResult = await auth().signInWithPhoneNumber(phoneNumber);
+      navigation.navigate(
+        'SendOTPRegisterScreen',
+        (data = {
+          verification: confirmationResult.verificationId,
+          numberPhone: phoneNumber,
+        }),
+      );
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Error', 'Failed to send OTP');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image style={styles.imageStyle} source={Icons.ImageForogtPassword} />
@@ -26,9 +60,11 @@ const ForgotPassword = ({navigation}) => {
         style={styles.textInput}
         placeholder="Enter the phone"
         placeholderTextColor={COLORS.color_7E7D7D}
+        onChangeText={Text => setPhoneNumber(Text)}
       />
       <View style={styles.formAction}>
-        <TouchableOpacity onPress={() => navigation.navigate('SendOtpScreen')}>
+        <TouchableOpacity
+          onPress={handerForgotPassword}>
           <View style={styles.btn}>
             <Text style={styles.btnText}>Continue</Text>
           </View>
@@ -60,7 +96,7 @@ const styles = StyleSheet.create({
   textStyle2: {
     fontSize: 14,
     color: COLORS.textHint,
-    marginTop: 15,
+marginTop: 15,
     marginBottom: 15,
   },
   textInput: {
