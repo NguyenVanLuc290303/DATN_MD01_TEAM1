1da0bf95f89cd98fe51d18f581c4b3099c602e30
admin <hieundph29367@fpt.edu.vn>
2024-03-07 14:54:52 +0700

    sửa API

--- src/config/api-consts.js
+++ src/config/api-consts.js
@@ -5,20 +5,20 @@ import { Children } from "react";
 var ip_address = "192.168.1.231";
 
 
-export const API_ADD_USERS = "http://192.168.1.9:3000/api-taikhoan";
+export const API_ADD_USERS = "http://192.168.1.5:3000/api-taikhoan";
 
-export const API_CATEGORY_PRODUCT = `http://192.168.1.9:3000/api-theloai`;
+export const API_CATEGORY_PRODUCT = `http://192.168.1.5:3000/api-theloai`;
 
-export const API_PRODUCT = "http://192.168.1.9:3000/api-sanpham";
+export const API_PRODUCT = "http://192.168.1.5:3000/api-sanpham";
 
-export const API_PRODUCT_TOP8 = "http://192.168.1.9:3000/api-sanpham/top8";
+export const API_PRODUCT_TOP8 = "http://192.168.1.5:3000/api-sanpham/top8";
 
-export const API_COLOR_PRODUCT = "http://192.168.1.9:3000/api-mausanpham";
+export const API_COLOR_PRODUCT = "http://192.168.1.5:3000/api-mausanpham";
 
 export const API_CHAT = "http://192.168.1.5:3000/api-tinnhan";
 
 
 
-export const API_ORDER = "http://192.168.0.100:3000/api-donhang";
+export const API_ORDER = "http://192.168.1.5:3000/api-donhang";
 
 
